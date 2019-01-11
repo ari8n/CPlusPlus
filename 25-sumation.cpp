3b075edebda7fe5dced634e263f6f0e3154628a6
Omid Lak Mazaheri <omidlmazaheri@gmail.com>
2019-01-11 21:27:14 +0330

    Create 25-sumation.cpp

---
+++ 25-sumation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+  int a=2,b;
+  b=++a;
+  
+  cout<<a ;
+  cout<<endl;
+  cout<<b;
+  
+                        
+  getch();
+}
