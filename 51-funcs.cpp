d235118973d787997a5eaff932186a66fdbc0b86
Omid Lak Mazaheri <omidlmazaheri@gmail.com>
2019-01-11 21:31:48 +0330

    Create 51-funcs.cpp

---
+++ 51-funcs.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+main()
+{
+  cout<<pow(2,3)<<endl;
+  cout<<pow(2,-2)<<endl;
+  
+  cout<<endl<<endl;
+  
+  cout<<fmod(6,4)<<endl;
+  cout<<fmod(-20,7)<<endl;
+
+  cout<<endl<<endl;
+
+  cout<<abs(-2)<<endl;
+  cout<<fabs(-3.5)<<endl;
+
+  cout<<endl<<endl;  
+    
+  cout<<floor(3.1)<<endl;
+  cout<<ceil(3.1)<<endl;
+  
+  cout<<endl<<endl;
+  
+ 
+  
+  cout<<char(tolower('A'))<<endl;
+ cout<<char(toupper('a'))<<endl;
+  
+  cout<<endl<<endl;
+  
+  cout<<islower('B')<<endl;
+  cout<<isupper('B')<<endl; 
+ 
+  cout<<endl<<endl;
+
+ 
+  cout<<int('a')<<endl;
+  cout<<char(97)<<endl;
+  
+  getch();
+}
