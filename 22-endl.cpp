e10ac387e07e778b67565208bf1e4b7fc34384c9
Omid Lak Mazaheri <omidlmazaheri@gmail.com>
2019-01-11 21:27:01 +0330

    Create 22-endl.cpp

---
+++ 22-endl.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+  int a;
+  a=5;
+   cout<<a++<<endl<<a<<'\n';
+   cout<<"*********\n";
+	cout<<++a<<endl<<a<<'\n';
+                        
+  getch();
+}
